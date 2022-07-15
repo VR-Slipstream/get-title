0764e2154d52e647a1a0fca8b5fd2180812b9538
VR-Slipstream <76850613+VR-Slipstream@users.noreply.github.com>
2022-07-15 10:47:37 +0000

    Update task_regex.py

--- task_regex.py
+++ task_regex.py
@@ -8,7 +8,7 @@ b = parser.parse_args()
 if "https://" in b.url or "http://" in b.url:
     html = requests.get(b.url, timeout=10).text
     title = re.findall(r"\<title\>(.*?)<\/title\>", html)
-    print(title[0])
+    print("Title:", title[0])
 
 else:
     print("Wrong Input. Look for -h/--help")
